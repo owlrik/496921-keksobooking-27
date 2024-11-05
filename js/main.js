ed6d7b016faa4dadeee9dc4103bae02740ff59bd
Vadim Tiuriumin <owlriks@gmail.com>
2024-11-05 17:12:04 +0800

    Добавил неактивное состояние форм при загрузке страницы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import {getOffers} from './data.js';
 import {renderCards} from './card.js';
+import {turnAdFormOff} from './form.js';
+import {turnFilterOff} from './filter.js';
 
 renderCards(getOffers());
+turnAdFormOff();
+turnFilterOff();
